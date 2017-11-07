6db7ba3ef6588aba15889e21c0381ed6fcc527bb
FabiW <f.wiche@gmx.de>
2017-11-07 22:01:28 +0100

    Header and Cpp files for data upload, logging and live data send added to Application layer

---
+++ Dongle/Application/Data_Upload.h
@@ -0,0 +1,6 @@
+#ifndef DATAUPLOAD_H
+#define DATAUPLOAD_H
+
+#include "../Interaction/Bluetooth.h"
+
+#endif //DATAUPLOAD_H
---
+++ Dongle/Application/Live_Data_Send.h
@@ -0,0 +1,6 @@
+#ifndef LIVE_DATA_SEND_H
+#define LIVE_DATA_SEND_H
+
+#include "../Interaction/Bluetooth.h"
+
+#endif //LIVE_DATA_SEND_H

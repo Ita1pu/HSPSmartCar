cc690841ade1f3be6484af9cd2f528cb4ed76d37
Franz Welker <franz.welker@web.de>
2017-11-16 19:23:18 +0100

    started to create odb class
    Signed-off-by: Franz Welker <franz.welker@web.de>

--- Dongle/lib/obd/src/obd.h
+++ Dongle/lib/obd/src/obd.h
@@ -2,5 +2,49 @@
 #define OBD_H
 
 
+#include <types.h>
+
+
+namespace obd {
+
+class ObdDevice
+{
+    public:
+        ObdDevice();
+        void initialize();
+
+        void updateVeryFastPids();
+        void updateFastPids();
+        void updateNormalPids();
+        void updateSlowPids();
+
+        ourTypes::smallPidVal getEngineCoolantTemp();
+        ourTypes::smallPidVal getEngineRpm();
+        ourTypes::smallPidVal getVehicleSpeed();
+        ourTypes::smallPidVal getRunTimeSineEngineStart();
+
+        ourTypes::smallPidVal getDistTraveledWithMalfuncIndicaLamp();
+        ourTypes::smallPidVal getFuelTankLvlInput();
+        ourTypes::smallPidVal getAbsBarometricPressure();
+
+
+        ourTypes::smallPidVal getAmbientAirTemp();
+        ourTypes::smallPidVal getFuelType();
+        ourTypes::smallPidVal getEthanolPercent();
+        ourTypes::smallPidVal getRelativAcceleratorPedalPos();
+        ourTypes::smallPidVal getEngineOilTemp();
+        ourTypes::smallPidVal getEngineFuelRate();
+        ourTypes::smallPidVal getDriverTorqueDemandEngine();
+        ourTypes::smallPidVal getActualTorqueEngine();
+        ourTypes::smallPidVal getEngineTorqueRef();
+        ourTypes::smallPidVal getEngineRunTime();
+
+
+    private:
+
+
+
+};
+};
 
 #endif
--- Dongle/lib/obd/src/signals.h
+++ Dongle/lib/obd/src/signals.h
@@ -4,70 +4,70 @@
 namespace obd
 {
     /**
-        Full name: Engine coolant temperature, Range:[-40; 215]°C
+        Full name: Engine coolant temperature, Range:[-40; 215]°C, Size: 1 byte
     */
     #define EngineCoolantTemp                    (0x5)
     /**
-        Full name: Engine RPM, Range: [0; 16 383.75]rpm
+        Full name: Engine RPM, Range: [0; 16 383.75]rpm, Size: 2 byte
     */
     #define EngineRpm                           (0x0C)
     /**
-        Full name: Vehicle speed, Range: [0; 255]km/h
+        Full name: Vehicle speed, Range: [0; 255]km/h, Size: 1 byte
     */
     #define VehicleSpeed                        (0x0D)
     /**
-        Full name: Run time since engine start, Range: [0; 65 535]seconds
+        Full name: Run time since engine start, Range: [0; 65 535]seconds, Size: 2 byte
     */
     #define RunTimeSineEngineStart              (0x1F)
 
     /**
-        Full name: Distance traveled with malfunction indicator lamp (MIL), Range: [0; 65 535]km
+        Full name: Distance traveled with malfunction indicator lamp (MIL), Range: [0; 65 535]km, Size: 2 byte
     */
     #define DistTraveledWithMalfuncIndicaLamp   (0x21)
     /**
-        Full name: Fuel tank level input, Range: [0; 100]%
+        Full name: Fuel tank level input, Range: [0; 100]%, Size: 1 byte
     */
     #define FuelTankLvlInput                    (0x2F)
     /**
-        Full name: Absolute barometric pressure, Range: [0; 255]kPa
+        Full name: Absolute barometric pressure, Range: [0; 255]kPa, Size: 1 byte
     */
     #define AbsBarometricPressure               (0x33)
 
     /**
-        Full name: Ambient ari temperature, Range: [-40; 215]°C
+        Full name: Ambient ari temperature, Range: [-40; 215]°C, Size: 1 byte
     */
     #define AmbientAirTemp                      (0x46)
     /**
-        Full name: Fuel type, Range: https://en.wikipedia.org/wiki/OBD-II_PIDs#Fuel_Type_Coding
+        Full name: Fuel type, Range: https://en.wikipedia.org/wiki/OBD-II_PIDs#Fuel_Type_Coding, Size: 1 byte
     */
     #define FuelType                            (0x51)
     /**
-        Full name: Ethanol fuel %, Range: [0; 100]%
+        Full name: Ethanol fuel %, Range: [0; 100]%, Size: 1 byte
     */
     #define EthanolPercent                      (0x52)
     /**
-        Full name: Relative accelerator pedal position, Range: [0; 100]%
+        Full name: Relative accelerator pedal position, Range: [0; 100]%, Size: 1 byte
     */
     #define RelativAcceleratorPedalPos          (0x5A)
     /**
-        Full name: Engine oil temperature, Range: [-40; 210]°C
+        Full name: Engine oil temperature, Range: [-40; 210]°C, Size: 1 byte
     */
     #define EngineOilTemp                       (0x5C)
     /**
-        Full name: Engine fuel rate, Range: [0; 3276.75]L/h
+        Full name: Engine fuel rate, Range: [0; 3 276.75]L/h, Size: 2 byte
     */
     #define EngineFuelRate                      (0x5E)
 
     /**
-        Full name: Driver's demand engine-percent torque, Range: [-125; 125]%
+        Full name: Driver's demand engine-percent torque, Range: [-125; 125]%, Size: 1 byte
     */
     #define DriverTorqueDemandEngine            (0x61)
     /**
-        Full name: Actual engine-percent torque, Range: [-125; 125]%
+        Full name: Actual engine-percent torque, Range: [-125; 125]%, Size: 1 byte
     */
     #define ActualTorqueEngine                  (0x62)
     /**
-        Full name: Engine reference torque, Range: [0; 65 535]Nm
+        Full name: Engine reference torque, Range: [0; 65 535]Nm, Size: 2 byte
     */
     #define EngineTorqueRef                     (0x63)
     /**

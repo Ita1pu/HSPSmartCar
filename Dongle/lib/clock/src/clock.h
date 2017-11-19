602642df716bf72801ed087086d0ef165c45ed2f
MSchleinkofer <michael.schleinkofer@st.oth-regensburg.de>
2017-11-19 15:41:39 +0100

    Issue #5: Clocking nearly implemented; To finish, the GPS must be done first!
    Signed-off-by: MSchleinkofer <michael.schleinkofer@st.oth-regensburg.de>

--- Dongle/lib/clock/src/clock.h
+++ Dongle/lib/clock/src/clock.h
@@ -3,10 +3,13 @@
 
 #include <stdint.h>
 
+#define CLOCK_RESOLUTION_MS 100
+
 /**
  *  Timing Class;
  *  This class provides methods for operations which have to do sth with time.
  *  E. g. setting timers and get the Time of day in UTC(GPS based).
+ *  Attention: Normally the Timer 2 is used to update the _timeOfDay
  */
 class Clock{
 public:
@@ -14,7 +17,7 @@ public:
    * Initialization Method Should be done at startup
    * @return Wether the initialization was successfull
    */
-  bool Init();
+  bool Initialize();
 
   /**
    * Receive the UTC time of day;
@@ -26,24 +29,38 @@ public:
    * Start a timer
    * @param nr the number of the timer to start
    * @param millis the number of milliseconds the timer should run
-   * @param flags the pointer to an integer where the bit specified by parameter nr is set to 1 after timer completion
+   * @param flags the pointer to an integer which is set to 1 after timer completion
    * @return Wether the setup was successfull
    */
-  bool SetTimer(uint8_t nr, uint32_t millis, uint8_t* flags);
+  bool SetTimer(uint8_t nr, uint16_t millis, uint8_t* flags);
 
   /**
    * Start a timer
+   * Does not work in parallel with SetTimeTicker
    * @param nr the number of the timer to start
    * @param millis the number of milliseconds the timer should run
    * @param callback the pointer to the function which shall be executed after timer completion
    * @return Wether the setup was successfull
    */
-  bool SetTimer(uint8_t nr, uint32_t millis, void (*callback)(void));
+  bool SetTimer(uint8_t nr, uint16_t millis, void (*callback)(void));
+
+  /**
+   * Set the flag in _flags
+   * This method should not be called by other methods or Functions.
+   */
+  void updateFlags();
+
+  /**
+   * Update the _timeOfDay
+   * This method should not be called by other methods or Functions.
+   */
+  void updateTime();
 protected:
 private:
-  uint8_t timeCounter; /** This variable counts how many times the time is updated without GPS*/
-  uint32_t timeOfDay;
+  uint32_t parseGPStoInt(int time);
+  uint8_t* _flags;
+  uint16_t _timeCounter; /** This variable counts how many times the time is updated without GPS*/
+  uint32_t _timeOfDay;
 };
-
 
 #endif

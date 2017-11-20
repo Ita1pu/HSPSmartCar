0ce289fb46c335447b142596c115f33c550d7636
Fabi <f.wiche@gmx.de>
2017-11-20 20:45:23 +0100

    VID 17 char vid added to our types

--- Dongle/lib/general/types.h
+++ Dongle/lib/general/types.h
@@ -1,12 +1,14 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <stdint.h>
 
 namespace ourTypes
 {
     typedef unsigned char byte;
     typedef char smallPidVal;
     typedef int bigPidVal;
+    typedef struct vid {char x[17];} vid;
 }
 
 #endif

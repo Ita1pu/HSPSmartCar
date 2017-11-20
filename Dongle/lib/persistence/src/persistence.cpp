2d970ac09dc9aad0b7ba8afd04025211132bc686
Fabi <f.wiche@gmx.de>
2017-11-20 20:46:50 +0100

    A fey functions added to persistence.h
    Doxygen documentation added to the files

--- Dongle/lib/persistence/src/persistence.cpp
+++ Dongle/lib/persistence/src/persistence.cpp
@@ -1,14 +1,91 @@
-#include "persistence.h"
+#include <persistence.h>
 
-char Persitence::init()
-{
-  this->status = 1;
-  this->card = new SDClass();
-  return card->begin(SD_CHIP_SELECT_PIN);
+Persistence::Persistence(const vid *current_vid, uint32_t current_time){
+  /** \brief The Constructor for the Logging Class
+    * Here all initalization for the class is done..
+    *
+    * \param *current_vid the Vehicle Identification number of the vehicle the software is running in
+    * \param current_time the time when the Logging starts
+    *
+  */
+  this->current_mvid = this->get_mapped_vehicle_id(current_vid);
+  this->open_file_date = current_time;
+  this->open_file = open_logging_file(current_time);
 }
 
-char Persitence::stop()
-{
-  return this->status = 0;
+bool Persistence::create_logging_entry(uint8_t mapped_vehicle_id, uint32_t time,
+  uint16_t data_id, uint32_t data_value){
+  /**\brief create a logging entry on the file_system
+    *
+    * One entry is written to the file_system.
+    * An entry looks like: **MVID_DATE_DATAID_VALUE**
+    *\param mapped_vehicle_id the mapped vehilce id which leads to the real one
+    *\param time the time for when the logging data was caputred
+    *\param data_id the id for the data to be logged
+    *\param data_value
+    *
+    *\return True if the logging was successful else False
+  */
+  return false;
+  }
 
+File Persistence::find_last_written_file(uint32_t current_time){
+  /** \brief open the last fiel which hast been written to the file system
+    *
+    * Search the filesystem for the last entry that has been written-
+    *
+    *\param current_time the current time
+    *
+    *\return the last written File
+  */
+  return nullptr_t;
+}
+
+File Persistence::create_logging_file(uint32_t current_time){
+  /** \brief create a new logfile
+    *
+    * Creates a new logfile in the fileystem according to the current time
+    * The file is named like the date it was created e.g. 01012017.log.
+    * It is stored in folder named after the corresponding mapped vehicle ID
+    *
+    * \param current_time the current time
+    *
+    * \return The created file
+  */
+
+  return nullptr_t;
+}
+
+uint8_t Persistence::get_mapped_vehicle_id(const vid *current_vid){
+  /** \Gets the mvid to the passed current_vid
+    *
+    * For memory space reasosn each VID is mapped to an mvid on the file system
+    * This function finds the MVID in the table and returns the mapped one
+    *
+    * \pram current_vid the VID of the car where the Dongle is used
+  */
+  return 0;
+}
+
+File Persistence::open_logging_file(uint32_t logging_start_time){
+  /** \brief opens the loggfile for the passed looging_start_time
+    *
+    * This function opens a file for the passed logging time. If no file is
+    * found then a new one will be created
+    *
+    * \param logging_start_time the time when the logging starts
+  */
+  return nullptr_t;
+}
+
+bool Persistence::update_file_name(uint32_t current_time){
+  /** \Checks if date has changed and creates a new file if neccessary
+    *
+    * When the vehicle was driving through midnight then a new file has to be
+    * created. This function checks it and creates a new file if neccessary
+    *
+    *\ param current_time the current time
+    *
+  */
+  return false;
 }
--- Dongle/lib/persistence/src/persistence.h
+++ Dongle/lib/persistence/src/persistence.h
@@ -1,19 +1,59 @@
+/**
+ * \class Persitence
+ *
+ *
+ * \brief Provide an example
+ *
+ * This class is meant as an example.  It is not useful by itself
+ * rather its usefulness is only a function of how much it helps
+ * the reader.  It is in a sense defined by the person who reads it
+ * and otherwise does not exist in any real form.
+ *
+ * \note Attempts at zen rarely work.
+ *
+ * \author (last to touch it) $Author: bv $
+ *
+ * \version $Revision: 1.5 $
+ *
+ * \date $Date: 2005/04/14 14:16:20 $
+ *
+ * Contact: bv@bnl.gov
+ *
+ * Created on: Wed Apr 13 18:39:37 2005
+ *
+ * $Id: doxygen-howto.html,v 1.5 2005/04/14 14:16:20 bv Exp $
+ *
+ */
 #ifndef PERSISTENCE_H
 #define PERSISTENCE_H
 
-#include <IFeature.h>
-#include <FreematicsBase.h>
-#include <FreematicsSD.h>
+#include <file_system_handler.h>
+#include <signals.h>
+#include <types.h>
 
-class Persitence : public IFeature {
+using namespace obd;
+using namespace ourTypes;
+
+class Persistence{
   public:
-    char init();
-    char stop();
-    //Persitence();
-
+    //Constructor
+    Persistence(const vid *current_vid, uint32_t current_time);
+    bool create_logging_entry(uint8_t mapped_vehicle_id, uint32_t time,
+                              uint16_t data_id, uint32_t data_value);
+    /*Getter*/
+    /*Setter*/
+  protected:
   private:
-    SDClass *card;
-};
+    uint8_t current_mvid = 0;
+    File open_file;
+    uint32_t open_file_date; //Has to change if logging passes 11:59 pm
+    File vid_map_table;
 
+    bool update_file_name(uint32_t current_time);
+    uint8_t get_mapped_vehicle_id(const vid *current_vid);
+    File find_last_written_file(uint32_t current_time);
+    File create_logging_file(uint32_t current_time);
+    File open_logging_file(uint32_t logging_start_time);
+};
 
 #endif

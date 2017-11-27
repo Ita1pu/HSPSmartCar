6edec3685b81cf22cdfc86dd920b3ad0cb6714e4
Fabi <f.wiche@gmx.de>
2017-11-27 19:36:01 +0100

    Test main file for compilation check.

--- Dongle/src/main.cpp
+++ Dongle/src/main.cpp
@@ -7,40 +7,24 @@
 #include <gps.h>
 #include <obd.h>
 #include <persistence.h>
+#include <types.h>
+#include <vid_mapper.h>
 
-Persistence *p;
-char str[100];
-char stat = 0;
-print(char *numstring, int number)
-{
-  Serial.print(numstring);
-  sprintf(str, "%x", number);
-  Serial.println(number);
-}
+
+
+persistence::Persistence *p = malloc(sizeof(persistence::Persistence));
+vid_mapper::Vid_mapper *mapper = malloc(sizeof(vid_mapper::Vid_mapper));
+const ourTypes::vid current_vid = {'A','1','B','2','C','3','D','4','E','5','F','6',
+                             'G','7','H','8','I',};
+uint32_t current_time = 0xDEADBEEF;
+
 void setup()
 {
-    p = new Persistence();
-    Serial.begin(9600);
-    print("SPI_MOSI_PIN: ", SPI_MOSI_PIN);
-    print("SPI_MISO_PIN: ", SPI_MISO_PIN);
-    print("SPI_SCK_PIN: ", SPI_SCK_PIN);
-    print("SD_CHIP_SELECT_PIN: ", SD_CHIP_SELECT_PIN);
-    Serial.println("Success");
-    // put your setup code here, to run once:
+    mapper = new vid_mapper::Vid_mapper(&current_vid);
+    p = new persistence::Persistence(&current_vid, current_time, mapper);
 }
 
 void loop()
 {
-    // if (p->getStatus())
-    // {
-    //   p->stop();
-    // }
-    // else
-    // {
-    //   p->init();
-    // }
-    // sprintf(str, "%x", p->getStatus());
-    // Serial.println(str);
-    // delay(500);
-    // put your main code here, to run repeatedly:
+
 }

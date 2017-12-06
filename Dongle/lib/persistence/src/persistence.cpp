c0aed70b552a361d6027330681050430fb7175f0
FabiW <f.wiche@gmx.de>
2017-12-06 12:44:00 +0100

    Comments added for doxygen

--- Dongle/lib/persistence/src/persistence.cpp
+++ Dongle/lib/persistence/src/persistence.cpp
@@ -4,15 +4,17 @@
 #include <time.h>
 using namespace persistence;
 
+/**
+ * @brief The Constructor for the Logging Class
+ * Here all initalization for the class is done..
+ * 
+ * @param current_vid the Vehicle Identification number of the vehicle the software is running in
+ * @param current_time the time when the Logging starts
+ * @param mapper The Mapp handler for VID-->MVID
+ * @param file_system The handler for the filesystem
+ */
 Persistence::Persistence(const vid *current_vid, uint32_t current_time,
                         Vid_mapper *mapper,File_System_Handler *file_system){
-  /** \brief The Constructor for the Logging Class
-    * Here all initalization for the class is done..
-    *
-    * \param *current_vid the Vehicle Identification number of the vehicle the software is running in
-    * \param current_time the time when the Logging starts
-    *
-  */
 
   this->_vid_mapper = mapper;
   this->_file_system = file_system;
@@ -20,49 +22,46 @@ Persistence::Persistence(const vid *current_vid, uint32_t current_time,
   this->_initStatus |= this->open_logging_file(current_time, &this->_open_file);
 }
 
+/**
+ * @brief Gets the mvid to the passed current_vid
+ * 
+ * @return stdRetVal 
+ */
 stdRetVal Persistence::set_mapped_vehicle_id()
 {
-   /** \Gets the mvid to the passed current_vid
-    *
-    * For memory space reasosn each VID is mapped to an mvid on the file system
-    * This function finds the MVID in the table and returns the mapped one
-    *
-    * \pram current_vid the VID of the car where the Dongle is used
-  */
   this->_vid_mapper->get_map_value(&this->_current_mvid);
   return NO_ERROR;
 }
 
+/**
+ * @brief create a logging entry in the current open file
+ * 
+ * @param time the  time when the value has been logged
+ * @param data_id The ID of the logdata
+ * @param data_value The value of the data
+ * @return stdRetVal 
+ */
 stdRetVal Persistence::create_logging_entry(uint32_t time,
   uint16_t data_id, uint32_t data_value){
-  /**\brief create a logging entry on the file_system
-    *
-    * One entry is written to the file_system.
-    * An entry looks like: **MVID_DATE_DATAID_VALUE**
-    *\param mapped_vehicle_id the mapped vehilce id which leads to the real one
-    *\param time the time for when the logging data was caputred
-    *\param data_id the id for the data to be logged
-    *\param data_value
-    *
-    *\return True if the logging was successful else False
-  */
   return NO_ERROR;
   }
 
+  /**
+   * @brief open the last fiel which hast been written to the file system
+   *
+   * Search the filesystem for the last entry that has been written
+   * 
+   * @param current_time the current time
+   * @param ret_file A pointer to the last wirrten file. Will be set by the class
+   * @return stdRetVal 
+   */
 stdRetVal Persistence::find_last_written_file(uint32_t current_time,
                                               File *ret_file){
-  /** \brief open the last fiel which hast been written to the file system
-    *
-    * Search the filesystem for the last entry that has been written-
-    *
-    *\param current_time the current time
-    *
-    *\return the last written File
-  */
+
   *ret_file = File();
   return NO_ERROR;
 }
-
+//TODO remove func
 stdRetVal Persistence::create_logging_file(uint32_t current_time,
                                               File *ret_file){
   /** \brief create a new logfile
@@ -79,15 +78,15 @@ stdRetVal Persistence::create_logging_file(uint32_t current_time,
   return NO_ERROR;
 }
 
-
+/**
+ * @brief opens the loggfile for the passed looging_start_time
+ * This function opens a file for the passed logging time. If no file is found then a new one will be created
+ * 
+ * @param logging_start_time The starting of the logging 
+ * @param ret_file TODO Remove
+ * @return stdRetVal 
+ */
 stdRetVal Persistence::open_logging_file(uint32_t logging_start_time, File *ret_file){
-  /** \brief opens the loggfile for the passed looging_start_time
-    *
-    * This function opens a file for the passed logging time. If no file is
-    * found then a new one will be created
-    *
-    * \param logging_start_time the time when the logging starts
-  */
   time_t logtime = logging_start_time;
   char file_path[13] = {0};
   char folder[4];
@@ -118,16 +117,15 @@ stdRetVal Persistence::open_logging_file(uint32_t logging_start_time, File *ret_
   this->_open_file.close();
   return NO_ERROR;
 }
-
+/**
+ * @brief Checks if date has changed and creates a new file if neccessary
+ * 
+ * When the vehicle was driving through midnight then a new file has to be
+ * created. This function checks it and creates a new file if neccessary
+ * @param current_time the current time
+ * @return stdRetVal 
+ */
 stdRetVal Persistence::update_file_name(uint32_t current_time){
-  /** \Checks if date has changed and creates a new file if neccessary
-    *
-    * When the vehicle was driving through midnight then a new file has to be
-    * created. This function checks it and creates a new file if neccessary
-    *
-    *\ param current_time the current time
-    *
-  */
   return NO_ERROR;
 }
 
@@ -135,9 +133,11 @@ stdRetVal Persistence::GetInitStatus()
 {
   return this->_initStatus;
 }
-
-void Persistence::setOpenFileDate(char *file_name)
-{/*!Assigns the first 5 chars of the filename to the date: E.g. 18JAN for 18th of January*/
+/**
+ * @brief Assigns the first 5 chars of the filename to the date: E.g. 18JAN for 18th of January
+ * 
+ */
+void Persistence::setOpenFileDate(char *file_name){
   for (int i = 0; i < SIZE_OF_CURRENT_DATE - 1; i++)
   {
     this->_open_file_date[i] = file_name[i];
--- Dongle/lib/persistence/src/vid_mapper.cpp
+++ Dongle/lib/persistence/src/vid_mapper.cpp
@@ -1,5 +1,5 @@
 #include <vid_mapper.h>
-using namespace vid_mapper;
+using namespace persistence;
 
 void Vid_mapper::set_map_file(File map_file)
 {
@@ -25,6 +25,9 @@ Vid_mapper::Vid_mapper(const vid *current_vid)
 
 stdRetVal Vid_mapper::get_map_value(uint8_t *mvid_result)
 {
+  //TODO Remove below
+  *mvid_result = 0xFE;
+  return 0x1A;
   uint8_t map_result = this->search_for_current_vid();
   if (map_result != NO_ERROR)
   {// VID has not been mapped in mapfile

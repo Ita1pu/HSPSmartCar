1596ed664a91cfad9954d8cf6a96cbbc2ec4ef43
MSchleinkofer <michael.schleinkofer@st.oth-regensburg.de>
2017-12-19 11:40:03 +0100

    Issue #6: API and first methods implemented!
    Signed-off-by: MSchleinkofer <michael.schleinkofer@st.oth-regensburg.de>

--- Dongle/lib/accReader/src/acc_reader.cpp
+++ Dongle/lib/accReader/src/acc_reader.cpp
@@ -1,1 +1,35 @@
 #include "acc_reader.h"
+
+bool AccReader::Initialize(){
+  _isFirst = true;
+  _sensor.memsInit(true);
+
+  _isInit = true;
+  return _isInit;
+}
+
+float AccReader::GetAcceleration(uint8_t ct){
+  if(ct >= 3 && _isInit == false)
+    return -999.999;
+
+  float accs[3];
+  bool success = _sensor.memsRead(accs, NULL, NULL, NULL, NULL);
+  if(success){
+    return accs[ct];
+  }else{
+    return -999.999;
+  }
+}
+
+float AccReader::GetAngle(uint8_t ct){
+  if(ct >= 3 && _isInit == false)
+    return -999.999;
+
+  float angs[3];
+  bool success = _sensor.memsRead(NULL, angs, NULL, NULL, NULL);
+  if(success){
+    return angs[ct];
+  }else{
+    return -999.999;
+  }
+}
--- Dongle/lib/accReader/src/acc_reader.h
+++ Dongle/lib/accReader/src/acc_reader.h
@@ -1,6 +1,57 @@
 #ifndef ACC_READER_H
 #define ACC_READER_H
 
+#include <FreematicsMEMS.h>
 
+/**
+ *  Intermediate Class for the Acceleration Sensor;
+ *  This class provides methods to get the Information about Position relative
+ *  to Ground and Acceleration.
+ */
+class AccReader{
+public:
+  /**
+   * Initialization Method Should be done at startup
+   * @return Wether the initialization was successfull
+   */
+  bool Initialize();
+
+  /**
+   * Getter for the acceleration data of one of the 3 axes
+   * @param  ct The number (0, 1 or 2) of the axis which is wanted
+   * @return The measured Data of the axis
+   */
+  float GetAcceleration(uint8_t ct);
+
+  /**
+   * Getter for the gyroscope data of one of the 3 axes
+   * @param ct The number (0, 1 or 2) of the axis which is wanted
+   * @return The measured Data of the gyroscope-axis
+   */
+  float GetAngle(uint8_t ct);
+
+  /**
+   * Getter for the collective Sensordata as a quarternion
+   * @param pointer to the float-array where to store the data;
+   * The Array must be at least 9 elements big. The size will not be checked.
+   * @return Boolean value if the operation was successfull and
+   * the quarternion is stored as following:
+   * quart[0] = Accel. x-Axis
+   * quart[1] = Accel. y-Axis
+   * quart[2] = Accel. z-Axis
+   * quart[3] = Gyro. x-Axis
+   * quart[4] = Gyro. y-Axis
+   * quart[5] = Gyro. z-Axis
+   * quart[6] = Magnet. x-Axis
+   * quart[7] = Magnet. y-Axis
+   * quart[8] = Magnet. z-Axis
+   */
+  bool GetQuarternion(float* qart);
+private:
+  bool _isFirst;
+  bool _isInit;
+  int8_t accCorrFactor;
+  MPU9250_9DOF _sensor;
+};
 
 #endif

4ca84dd4f873a83d2e3ace2294af1e8414d292d6
Ita1pu <18101323+Itiapu@users.noreply.github.com>
2018-03-21 22:12:10 +0100

    Added  functions for reading send buffer and date for bluetooth upload to ApP

--- Dongle/lib/bluetooth/src/bluetooth.cpp
+++ Dongle/lib/bluetooth/src/bluetooth.cpp
@@ -40,3 +40,47 @@ using namespace bluetooth;
   BtUploader::~BtUploader(){
     this->log_upload_postion();
   }
+
+  void BtUploader::read_upload_size_and_date(){
+    uint8_t i = 0;
+    uint8_t in[IN_BUFFER] = {0};
+
+    //Recive Request like: "##<size>"
+    Serial.readBytes(in, IN_BUFFER);
+
+    while(i < IN_BUFFER){
+      if(in[i] == '#' and in[i+1] == '#'){
+        Serial.print("##");
+        Serial.println(in[i+2]);
+        upload_size = in[i+2];
+        break;
+      }
+      i++;
+    }
+    in[IN_BUFFER] = {0}; //Clear buffer
+    delay(MAX_RESPONSE_DELAY);
+    Serial.readBytes(in, IN_BUFFER);//Read date like ;;20101999
+
+    while ( i < IN_BUFFER){
+      if(in[i] == ';' and in[i+1] == ';'){
+        if (i <= IN_BUFFER - 10){
+          this->date[0] = in[i+2];
+          this->date[1] = in[i+3];
+          this->date[2] = in[i+4];
+          this->date[3] = in[i+5];
+          this->date[4] = in[i+6];
+          this->date[5] = in[i+7];
+          this->date[6] = in[i+8];
+          this->date[7] = in[i+9];
+          this->date[8] = 0;
+          Serial.print(";;");
+          Serial.println(this->date);
+          break;
+        }
+        else{
+          Serial.println("No date received starting with oldest one");
+        }
+      }
+      i++;
+    }
+  }
--- Dongle/lib/bluetooth/src/bluetooth.h
+++ Dongle/lib/bluetooth/src/bluetooth.h
@@ -3,12 +3,18 @@
 
 #include <persistence.h>
 namespace bluetooth{
+#define IN_BUFFER             10
+#define MAX_RESPONSE_DELAY    1000
+#define SIZE_OF_DATE_STRING   9       //28102018\0
 
 class BtUploader{
   public:
   void log_upload_postion();
   void upload_bt();
   void init(persistence::Persistence *p);
+  void read_upload_size_and_date();
+  char date[SIZE_OF_DATE_STRING] = "";
+  uint8_t upload_size = 0;
   ~BtUploader();
 private:
   uint8_t _current_car;

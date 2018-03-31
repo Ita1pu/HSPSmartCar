2d67ec0db793f7972221f30d1ee143e842916b50
Andreas Lackner <alackner@outlook.com>
2018-03-31 17:12:51 +0200

    Extended trip summary calculation

--- Backend/SmartCar/SmartCarApi/Statistics/Advanced/SummaryStatistic.cs
+++ Backend/SmartCar/SmartCarApi/Statistics/Advanced/SummaryStatistic.cs
@@ -13,9 +13,22 @@ namespace SmartCarApi.Statistics.Advanced
         {
             var summary = new TripSummary();
 
-
+            summary.Summary = GetSummary(trips);
+            summary.DistanceHistogram = GetDistanceHistogram(trips);
 
             return summary;
+        }
+
+        private Dictionary<string, SummaryEntry> GetSummary(List<Trip> trips)
+        {
+            var tripGroups = trips.GroupBy(t => t.TripStart.Month);
+            return null;
+        }
+
+        private List<Tuple<string, double>> GetDistanceHistogram(List<Trip> trips)
+        {
+
+            return null;
         }
     }
 }
